7cd61aed98f822c0e401603e48a22da10b732ee9
Jitendra Bangani <jtbangan@ncsu.edu>
2012-09-30 14:02:11 -0400

    Added error notifications

--- backupwireless/views.py
+++ backupwireless/views.py
@@ -1,4 +1,5 @@
 from subprocess import Popen
+from django.shortcuts import render
 from django.template import RequestContext
 from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
@@ -8,13 +9,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from backupwireless.models import AccessPoint, WirelessNetwork
 
 class HomeView(TemplateView):
+    
     template_name = "backupwireless/home.html"
-
+    
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         online = WirelessNetwork.objects.all()[0].online
         context.update({"statusText": 'Enabled' if online else 'Disabled'})
         context.update({"buttonText": 'Disable' if online else 'Enable'})
+        context.update({"unreachableHosts": self.request.session.pop('unreachable_hosts','start').strip().rstrip(',')})
         return context
         
     def dispatch(self, *args, **kwargs):
@@ -22,26 +25,38 @@ class HomeView(TemplateView):
 
 @login_required
 def switch(request):
+	unreachable_hosts = ""	
 	if WirelessNetwork.objects.all()[0].online:
 		for ap in AccessPoint.objects.all():
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'nvram', 'set', 'ath0_net_mode=disabled']) 
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'nvram', 'set', 'ath0_net_mode=disabled']) 
 			exitCode = pHandle.wait()
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'nvram', 'commit']) 
+			if exitCode != 0:
+					unreachable_hosts += ap.ip + ","
+					continue
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'nvram', 'commit']) 
+			exitCode = pHandle.wait()		
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'reboot']) 
 			exitCode = pHandle.wait()
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'reboot']) 
-			exitCode = pHandle.wait()
-			# Log the IP for which the SSH failed?
 		WirelessNetwork.objects.all().update(online=0)
 	else:
 		for ap in AccessPoint.objects.all():
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'nvram', 'set', 'ath0_net_mode=mixed']) 
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'nvram', 'set', 'ath0_net_mode=mixed']) 
 			exitCode = pHandle.wait()
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'nvram', 'commit']) 
+			if exitCode != 0:
+					unreachable_hosts += ap.ip + ","
+					continue
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'nvram', 'commit']) 
 			exitCode = pHandle.wait()
-			pHandle = Popen(['ssh', 'root@' + ap.ip, 'reboot']) 
-			# Log the IP for which the SSH failed?
+			pHandle = Popen(['ssh', '-o ConnectTimeout=1', 'root@' + ap.ip, 'reboot']) 
+			exitCode = pHandle.wait()
 		WirelessNetwork.objects.all().update(online=1)
-	return HttpResponseRedirect("/");
+	
+	if unreachable_hosts:
+		request.session['unreachable_hosts'] = unreachable_hosts
+	else:
+		request.session['unreachable_hosts'] = "none"
+
+	return HttpResponseRedirect("/")
 
 @login_required
 def PassReset(request):
